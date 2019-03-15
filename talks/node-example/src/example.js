2c280119da6c1b1e3c1dcb9f586089ee22928ace
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-15 09:07:42 +0000

    Deploy terrestris/fossgis2019 to github.com/terrestris/fossgis2019.git:gh-pages

--- talks/node-example/src/example.js
+++ talks/node-example/src/example.js
@@ -3,17 +3,20 @@ import View from 'ol/View.js';
 import TileLayer from 'ol/layer/Tile.js';
 import TileWMS from 'ol/source/TileWMS.js';
 
+import OSM from 'ol/source/OSM.js';
 
-var wmsSource = new TileWMS({
-  url: 'https://ahocevar.com/geoserver/wms',
-  params: {'LAYERS': 'ne:ne', 'TILED': true},
-  serverType: 'geoserver',
-  crossOrigin: 'anonymous'
+
+
+var wmsSource2 = new TileWMS({
+  url: 'http://localhost:8888/geoserver/wms',
+  params: {'LAYERS': 'mj:mj', 'TILED': true}
 });
 
-var wmsLayer = new TileLayer({
-  source: wmsSource
+
+var wmsLayer2 = new TileLayer({
+  source: wmsSource2
 });
+
 
 var view = new View({
   center: [0, 0],
@@ -21,30 +24,9 @@ var view = new View({
 });
 
 var map = new Map({
-  layers: [wmsLayer],
+  layers: [new TileLayer({
+    source: new OSM()
+  }), wmsLayer2],
   target: 'map',
   view: view
-});
-
-map.on('singleclick', function(evt) {
-  document.getElementById('info').innerHTML = '';
-  var viewResolution = /** @type {number} */ (view.getResolution());
-  var url = wmsSource.getGetFeatureInfoUrl(
-    evt.coordinate, viewResolution, 'EPSG:3857',
-    {'INFO_FORMAT': 'text/html'});
-  if (url) {
-    document.getElementById('info').innerHTML =
-        '<iframe seamless src="' + url + '"></iframe>';
-  }
-});
-
-map.on('pointermove', function(evt) {
-  if (evt.dragging) {
-    return;
-  }
-  var pixel = map.getEventPixel(evt.originalEvent);
-  var hit = map.forEachLayerAtPixel(pixel, function() {
-    return true;
-  });
-  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
 });
